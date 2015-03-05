15cf695b94fafb5802596db1c460cdba23e1e126
otiai10 <otiai10@gmail.com>
2015-03-05 17:48:56 +0900

    Add `clear`

--- js/main.js
+++ js/main.js
@@ -7,9 +7,14 @@ $(function(){
     $input.on('keydown',function(ev) {
         if (/* !ev.shiftKey || */ev.which != 13) return;
         // feedback
-        var el = '<div class="promt-row"><span class="prompt"></span><span id="prompt-input" contenteditable="true"></span><span>' + ev.currentTarget.innerHTML + '</span></div>';
+        var el = '<div class="prompt-row prompt-appended"><span class="prompt"></span><span id="prompt-input" contenteditable="true"></span><span>' + ev.currentTarget.innerHTML + '</span></div>';
         $(el).insertBefore($input.parent());
         // special
+        if (ev.currentTarget.innerHTML == "clear") {
+            $("div.prompt-row.prompt-appended", document).remove();
+            $input.html('');
+            return;
+        }
         var al = ev.currentTarget.innerHTML.match(/^hello\((.+)\);?$/);
         $input.html('');
         if (al && al.length > 1) {
